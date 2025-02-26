3ed633feb77f1daa0aeedf2b7369a650ad5944b7
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-26 15:44:15 +0600

    User model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
