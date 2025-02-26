b1e8444f554dd9d714ab243c5f8f92e4b446346d
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-26 17:32:52 +0600

    Views: register-user

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
