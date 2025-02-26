b400d9691a2bc363a0945ddff60ea007d43f4f73
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-26 17:48:34 +0600

    Views: login-user

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
 
@@ -14,3 +17,16 @@ def register_user(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user:
+        token, _ = Token.objects.get_or_create(user=user)  # Create/Fetch token
+        return Response({"token": token.key, "message": "Login successful"}, status=status.HTTP_200_OK)
+
+    return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
